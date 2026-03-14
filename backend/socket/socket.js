678aa50f4ebdb8b6a65b6540a80535bb0de9014d
Sneha Gupta <snehagupta8765@gmail.com>
2026-03-14 20:23:37 +0530

    fixing route issues

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const initSocket = (server) => {
 
     io = new Server(server, {
         cors: {
-            origin: ["http://localhost:5173", "https://your-frontend-url.vercel.app"],
+            origin: "https://chat-application-1n71.onrender.com",
             methods: ["GET", "POST"]
         },
         transports: ["websocket"]
