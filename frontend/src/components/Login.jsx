5bd74899e084e34a0de149a894336d9d4962088c
Sneha Gupta <snehagupta8765@gmail.com>
2026-03-14 16:08:30 +0530

    fixing login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -7,7 +7,6 @@ import { useDispatch } from "react-redux";
 import { setAuthUser } from "../redux/userSlice";
 import { connectSocket } from "../socket";
 
-
 const Login = () => {
   const dispatch = useDispatch();
 
@@ -24,7 +23,7 @@ const Login = () => {
       const res = await axios.post(
         "https://chat-application-1n71.onrender.com/api/v1/user/login",
         user,
-        { withCredentials: true }
+        { withCredentials: true },
       );
 
       console.log("Response:", res.data);
@@ -33,39 +32,33 @@ const Login = () => {
       toast.success("Login Successful 🎉");
 
       dispatch(setAuthUser(res.data));
-      
-      connectSocket(res.data.user._id);  // 
-      navigate("/");   // or navigate("/home") 
 
+      connectSocket(res.data._id);
+
+      navigate("/"); // or navigate("/home")
     } catch (error) {
       console.log(error);
 
       // ❌ Error Toast
-      toast.error(
-        error.response?.data?.message || "Login Failed ❌"
-      );
+      toast.error(error.response?.data?.message || "Login Failed ❌");
     }
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="w-96 p-8 bg-gray-400/10 backdrop-blur-md border border-gray-200 rounded-xl shadow-lg">
-
         <h1 className="text-3xl font-bold text-center text-green-300 mb-6">
           Login
         </h1>
 
         <form onSubmit={onSubmitHandler} className="space-y-4 text-left">
-
           <div>
             <label className="block text-sm font-medium text-black mb-1">
               Username
             </label>
             <input
               value={user.userName}
-              onChange={(e) =>
-                setUser({ ...user, userName: e.target.value })
-              }
+              onChange={(e) => setUser({ ...user, userName: e.target.value })}
               type="text"
               placeholder="email"
               className="w-full input input-bordered h-10 text-white"
@@ -78,9 +71,7 @@ const Login = () => {
             </label>
             <input
               value={user.password}
-              onChange={(e) =>
-                setUser({ ...user, password: e.target.value })
-              }
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
               type="password"
               placeholder="Password"
               className="w-full input input-bordered h-10 text-white"
